0811fbb3f87542d5fb83e65cd42fb8cb27329d6f
Matt Nguyen <mnguyen916@gmail.com>
2014-05-05 22:38:51 +0700

    Clean up twittalytics.pm file

--- lib/twittalytics.pm
+++ lib/twittalytics.pm
@@ -1,50 +1,19 @@
 package Twittalytics;
+
 use Dancer ':syntax';
 use strict;
 use warnings;
 use Cwd;
 use Sys::Hostname;
-use examples::simple_form;
-use examples::navbar_login;
-use examples::tabs;
-use examples::show_file;
-use examples::photo_gallery;
-use examples::markdown;
-use examples::template_plugins;
-use examples::error_handling;
-use examples::dynamic_content;
+
+# Services
+use Services::TwitterClient;
 
 our $VERSION = '0.1';
 
 get '/' => sub {
-    template 'index';
-};
-
-get '/deploy' => sub {
-    template 'deployment_wizard', {
-		directory => getcwd(),
-		hostname  => hostname(),
-		proxy_port=> 8000,
-		cgi_type  => "fast",
-		fast_static_files => 1,
-	};
-};
-
-#The user clicked "updated", generate new Apache/lighttpd/nginx stubs
-post '/deploy' => sub {
-    my $project_dir = param('input_project_directory') || "";
-    my $hostname = param('input_hostname') || "" ;
-    my $proxy_port = param('input_proxy_port') || "";
-    my $cgi_type = param('input_cgi_type') || "fast";
-    my $fast_static_files = param('input_fast_static_files') || 0;
-
-    template 'deployment_wizard', {
-		directory => $project_dir,
-		hostname  => $hostname,
-		proxy_port=> $proxy_port,
-		cgi_type  => $cgi_type,
-		fast_static_files => $fast_static_files,
-	};
+  my $twitter_client = Services::TwitterClient->new;
+  return $twitter_client->fetch_bearer_token;
 };
 
 true;
