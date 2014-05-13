3fe4a5d0a24a22e67760f11e98a0fe0293fbd091
Matt Nguyen <mnguyen916@gmail.com>
2014-05-13 15:37:57 +0700

    Update styling

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -3,15 +3,25 @@ var controllers = angular.module('twittalytics.controllers', []);
 controllers.controller("HomeController", ["$scope", function($scope) {
 }]);
 
+controllers.controller("NavigationController", ["$scope", "$location", function($scope, $location) {
+  $scope.isActive = function(path) {
+    if ($location.path() == path) {
+      return "active";
+    } else {
+      return "";
+    }
+  };
+}])
+
 controllers.controller("CommonFriendsController", ["$scope", "CommonFriends", function($scope, CommonFriends) {
   $scope.getCommonFriends = function() {
     CommonFriends($scope.username1, $scope.username2)
       .then(function(response) {
         $scope.commonFriends = response.data.common_friends;
       }, function(error) {
-        console.log('nooo');
+        console.log(error);
       });
-  }
+  };
 }]);
 
 controllers.controller("StatusesController", ["$scope", "RecentStatuses", function($scope, RecentStatuses) {
@@ -20,7 +30,7 @@ controllers.controller("StatusesController", ["$scope", "RecentStatuses", functi
       .then(function(response) {
         $scope.recentStatuses = response.data.tweets;
       }, function(error) {
-        console.log('nooo');
+        console.log(error);
       });
-  }
+  };
 }]);
